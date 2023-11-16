fb56115c84a7d8aba953c3311ae5cbaaf02b10e6
MWise707 <markezrawiseman@gmail.com>
2023-11-15 21:05:02 -0800

    Add gradient checkerboard

--- script.js
+++ script.js
@@ -11,30 +11,33 @@ containerEl.style.height = "800px";
 containerEl.style.color = "blue";
 
 // Tile Functions
-function createRedTile() {
-  let redTile = document.createElement("div");
-  redTile.style.width = "12.5%";
-  redTile.style.height = "12.5%";
-  redTile.style.backgroundColor = "red";
-  return redTile;
+function createDynamicTile(color) {
+  let dynamicTile = document.createElement("div");
+  dynamicTile.style.width = "12.5%";
+  dynamicTile.style.height = "12.5%";
+  dynamicTile.style.backgroundColor = color;
+  return dynamicTile;
 }
 
-function createBlackTile() {
-  let blackTile = document.createElement("div");
-  blackTile.style.width = "12.5%";
-  blackTile.style.height = "12.5%";
-  blackTile.style.backgroundColor = "black";
-  return blackTile;
-}
+const createRedTile = function () {
+  createDynamicTile("red");
+};
+const createBlackTile = function () {
+  createDynamicTile("black");
+};
+const addElToCont = function (element) {
+  containerEl.appendChild(element);
+};
+
 
 function createOddRow() {
   for (var i = 1; i < 9; i++) {
     if (i % 2 !== 0) {
-      let redTile = createRedTile();
-      containerEl.appendChild(redTile);
+      let redTile = createDynamicTile("red");
+      addElToCont(redTile);
     } else {
-      let blackTile = createBlackTile();
-      containerEl.appendChild(blackTile);
+      let blackTile = createDynamicTile("black");
+      addElToCont(blackTile);
     }
   }
 }
@@ -42,11 +45,11 @@ function createOddRow() {
 function createEvenRow() {
   for (var i = 1; i < 9; i++) {
     if (i % 2 !== 0) {
-      let blackTile = createBlackTile();
-      containerEl.appendChild(blackTile);
+      let blackTile = createDynamicTile("black");
+      addElToCont(blackTile);
     } else {
-      let redTile = createRedTile();
-      containerEl.appendChild(redTile);
+      let redTile = createDynamicTile("red");
+      addElToCont(redTile);
     }
   }
 }
@@ -91,8 +94,52 @@ function createRandTile() {
 const createRandoCheckerboard = function () {
   for (let i = 1; i < 65; i++) {
     let randoTile = createRandTile();
-    containerEl.appendChild(randoTile)
+    containerEl.appendChild(randoTile);
   }
 };
-createRandoCheckerboard();
+// createRandoCheckerboard();
 
+// On a new branch called `gradient`, write code to color the tiles using some sort of gradient.  It does not need to look exactly like the image below, but it should have some sort of increasing/decreasing color values.
+
+const colorGradients = {
+  yellowOrange: ["#FFFFCC", "#FFFF99", "#FFFF66", "#FFFF33", "#FFFF00", "#FFCC66", "#FFCC99", "#FFCC33", "#FFCC00",  "#FF9966", "#FF9933", "#FF9900"],
+  redGradient: ["#FF6666", "#FF6633", "#FF6600", "#CC6666", "#CC3333", "#CC3300", "#FF3333", "#FF3300", "#FF0033", "#FF0000", "#CC0033", "#CC0000", "#993333", "#993300", "#990033", "#990000", "#990066", "#993366"],
+  redPinkPurple: ["#CC3366", "#CC0066", "#FF3366", "#FF0066", "#FF3399", "#FF0099", "#FFCCCC", "#CC9999", "#FF9999", "#FF6699", "#FF99CC", "#FF66CC", "#FFCCFF", "#FF99FF", "#FF66FF", "#FF33FF", "#FF00FF", "#FF33CC",  "#CC99CC", "#CC66CC", "#CC6699", "#CC3399", "#CC0099", "#FF00CC", "#CC99FF", "#CC66FF", "#CC33FF", "#CC00FF", "#CC00CC", "#CC33CC", "#9966CC", "#996699", "#9933CC", "#9900CC", "#993399", "#990099", "#9999FF", "#9966FF", "#9933FF", "#9900FF ", "#660099", "#663399"], 
+}
+
+console.log("redPinkPurple " + colorGradients.redPinkPurple.length);
+console.log("yellowOrange " + colorGradients.yellowOrange.length);
+console.log("redGradient " + colorGradients.redGradient.length);
+
+function createYellowOrangeRow () {
+  for (let i = 0; i < 12; i++) {
+    let tileColor = colorGradients.yellowOrange[i];
+    let gradientTile = createDynamicTile(tileColor);
+    addElToCont(gradientTile);
+  }
+}
+function createRedGradRow () {
+  for (let i = 0; i < 18; i++) {
+    let tileColor = colorGradients.redGradient[i];
+    let gradientTile = createDynamicTile(tileColor);
+    addElToCont(gradientTile);
+  }
+}
+function createRedPinkPurple () {
+  for (let i = 0; i < 34; i++) {
+    let tileColor = colorGradients.redPinkPurple[i];
+    let gradientTile = createDynamicTile(tileColor);
+    addElToCont(gradientTile);
+  }
+}
+
+function createGradientBoard() {
+  createYellowOrangeRow();
+  createRedGradRow();
+  createRedPinkPurple();
+}
+
+// createGradientBoard();
+
+
+git 
